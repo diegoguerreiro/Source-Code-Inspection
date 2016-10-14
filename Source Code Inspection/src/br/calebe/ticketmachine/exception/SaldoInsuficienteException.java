ef579df64b7f81575a0804238cf58d8390e9626e
diegoguerreiro <diego0guerreiro@gmail.com>
2016-10-13 21:11:00 -0300

    TicketMachine.java
    Add Author

--- Source Code Inspection/src/br/calebe/ticketmachine/exception/SaldoInsuficienteException.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/exception/SaldoInsuficienteException.java
@@ -2,7 +2,7 @@ package br.calebe.ticketmachine.exception;
 
 /**
  *
- * @author Calebe de Paula Bianchini
+ * @author Diego Araujo
  */
 public class SaldoInsuficienteException extends Exception {
 }
